89b11613d2c4e2555476d52306a97a4a960320be
Thegrep01 <den.belotserkovets@gmail.com>
2019-04-03 12:34:51 +0300

    added demo
    Signed-off-by: Raphael Morineau <raphael.morineau@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Rules
 The player will see a map of Europe without Streets + Cities (Only Country X borders). Its mission is to find the right location to the city name on this map. After placing the needle pin, the game will show you the right location of the city and the difference of your needle pin and the city in kilometres. If it is in around 50km of the city, the selection will be defined as "correct".
+
+## Preview: 
+
+<img src="./assets/gifs/demo.gif" width="320" height="640">
--- src/app/game.component.tsx
+++ src/app/game.component.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import MapView, { MapEvent, Marker } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
 import data from './game.data.json';
 
-interface IMarker { lat: number; lon: number; title?: string; }
+interface IMarker {
+    lat: number;
+    lon: number;
+    title?: string;
+}
 
 interface IState {
     region: {
-        latitude: number,
-        longitude: number,
-        latitudeDelta: number,
-        longitudeDelta: number
+        latitude: number;
+        longitude: number;
+        latitudeDelta: number;
+        longitudeDelta: number;
     };
     kilometers: number;
     score: number;
@@ -19,146 +24,147 @@ interface IState {
         pos: {
             lat: number;
             lng: number;
-        }
+        };
     };
     markers?: IMarker[];
 }
 
-export class Game extends React.Component<{}, IState> {
-    public constructor(props: {}) {
-        super(props);
-        this.state = {
-            region: {
-                latitude: 46.6765288,
-                longitude: 14.6822894,
-                latitudeDelta: 60.9,
-                longitudeDelta: 5
-            },
-            kilometers: 1500,
-            score: 0
-        };
-    }
+const initialState: IState = {
+    region: {
+        latitude: 46.6765288,
+        longitude: 14.6822894,
+        latitudeDelta: 60.9,
+        longitudeDelta: 5,
+    },
+    kilometers: 1500,
+    score: 0,
+};
 
+export function Game() {
+    const [state, setState] = useState(initialState);
 
-    public setMarkers = (lat1: number, lon1: number, lat2: number, lon2: number, title: string) => {
-        this.setState({
-            markers: [{ lat: lat1, lon: lon1 }, { lat: lat2, lon: lon2, title }]
-        });
+    useEffect(() => {
+        if (state.kilometers <= 0) {
+            Alert.alert('Game Over', `Your score is ${state.score}`, [
+                { text: 'Restart', onPress: () => restart() },
+            ]);
+        }
+    }, [state.kilometers]);
+
+    const setMarkers = (lat1: number, lon1: number, lat2: number, lon2: number, title: string) => {
+        setState(prevState => ({
+            ...prevState,
+            markers: [{ lat: lat1, lon: lon1 }, { lat: lat2, lon: lon2, title }],
+        }));
     };
 
-    public restart = () => {
-        this.setState({
+    const restart = () => {
+        setState(prevState => ({
+            ...prevState,
             kilometers: 1500,
             score: 0,
             markers: undefined,
-            city: undefined
-        });
+            city: undefined,
+        }));
     };
 
-    public componentDidUpdate(): void {
-        if (this.state.kilometers <= 0) {
-            Alert.alert('Game Over', `Your score is ${this.state.score}`, [
-                { text: 'Restart', onPress: () => this.restart() }
-            ]);
-        }
-    }
-
-    public handleMapPress = (event: MapEvent) => {
-        if (!this.state.city) {
+    const handleMapPress = (event: MapEvent) => {
+        if (!state.city) {
             Alert.alert('Choose city');
             return;
         }
         const { latitude, longitude } = event.nativeEvent.coordinate;
-        const diff: number = this._distance(latitude, longitude, this.state.city.pos.lat, this.state.city.pos.lng);
+        const diff = _distance(latitude, longitude, state.city.pos.lat, state.city.pos.lng);
         if (diff <= 50) {
-            this.setState({ score: this.state.score + 1 });
+            setState(prevState => ({ ...prevState, score: state.score + 1 }));
         } else {
-            this.setState({
+            setState(prevState => ({
+                ...prevState,
                 kilometers:
-                    Number(Number(this.state.kilometers - diff).toFixed(0)) <= 0
+                    Number(Number(state.kilometers - diff).toFixed(0)) <= 0
                         ? 0
-                        : Number(Number(this.state.kilometers - diff).toFixed(0))
-            });
+                        : Number(Number(state.kilometers - diff).toFixed(0)),
+            }));
         }
-        this.setMarkers(latitude, longitude, this.state.city.pos.lat, this.state.city.pos.lng, this.state.city.name);
+        setMarkers(latitude, longitude, state.city.pos.lat, state.city.pos.lng, state.city.name);
     };
 
-    public render(): JSX.Element {
-        return (
-            <View style={styles.container}>
-                <View style={styles.hint}>
-                    <Text>{this.state.score}</Text>
-                </View>
-                <View style={styles.hint}>
-                    <Text>{this.state.kilometers}</Text>
-                </View>
-                <View style={styles.step}>
-                    <Text>Select the location of</Text>
-                    <Text style={{ fontWeight: 'bold' }}>{this.state.city && this.state.city.name}</Text>
-                </View>
-                <MapView
-                    style={styles.map}
-                    zoomEnabled={false}
-                    scrollEnabled={false}
-                    initialRegion={this.state.region}
-                    onPress={this.handleMapPress}
-                >
-                    {this.state.markers && this.state.markers.map((marker: IMarker, index: number) => (
+    const onPressPlace = () => {
+        const randomCity = data.cities[Math.floor(Math.random() * data.cities.length)];
+        setState(prevState => ({
+            ...prevState,
+            city: {
+                name: randomCity.name,
+                pos: randomCity.position,
+            },
+            markers: [],
+        }));
+    };
+
+    const _distance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+        if (lat1 === lat2 && lon1 === lon2) {
+            return 0;
+        }
+
+        const radlat1 = (Math.PI * lat1) / 180;
+        const radlat2 = (Math.PI * lat2) / 180;
+        const theta = lon1 - lon2;
+        const radtheta = (Math.PI * theta) / 180;
+        let dist =
+            Math.sin(radlat1) * Math.sin(radlat2) +
+            Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = (dist * 180) / Math.PI;
+        dist = dist * 60 * 1.1515;
+        return dist;
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.hint}>
+                <Text>{state.score}</Text>
+            </View>
+            <View style={styles.hint}>
+                <Text>{state.kilometers}</Text>
+            </View>
+            <View style={styles.step}>
+                <Text>Select the location of</Text>
+                <Text style={{ fontWeight: 'bold' }}>{state.city && state.city.name}</Text>
+            </View>
+            <MapView
+                style={styles.map}
+                zoomEnabled={false}
+                scrollEnabled={false}
+                initialRegion={state.region}
+                onPress={handleMapPress}
+            >
+                {state.markers &&
+                    state.markers.map((marker: IMarker, index: number) => (
                         <Marker
                             key={index}
                             coordinate={{
                                 latitude: marker.lat,
-                                longitude: marker.lon
+                                longitude: marker.lon,
                             }}
                             title={marker.title || 'Your choose'}
                         />
                     ))}
-                </MapView>
-                <View style={styles.action}>
-                    <Button
-                        onPress={this.onPressPlace}
-                        title='Place'
-                        color='#999'
-                        accessibilityLabel='Place the marker for a city'
-                    />
-                </View>
+            </MapView>
+            <View style={styles.action}>
+                <Button
+                    onPress={onPressPlace}
+                    title='Place'
+                    color='#999'
+                    accessibilityLabel='Place the marker for a city'
+                />
             </View>
-        );
-    }
-
-    public onPressPlace = () => {
-        const i: number = Math.floor(Math.random() * data.cities.length);
-        this.setState({
-            city: {
-                name: data.cities[i].name,
-                pos: data.cities[i].position
-            },
-            markers: []
-        });
-    };
-
-    private _distance(lat1: number, lon1: number, lat2: number, lon2: number): number {
-        if (lat1 === lat2 && lon1 === lon2) {
-            return 0;
-        } else {
-            const radlat1: number = (Math.PI * lat1) / 180;
-            const radlat2: number = (Math.PI * lat2) / 180;
-            const theta: number = lon1 - lon2;
-            const radtheta: number = (Math.PI * theta) / 180;
-            let dist: number =
-                Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-            if (dist > 1) {
-                dist = 1;
-            }
-            dist = Math.acos(dist);
-            dist = (dist * 180) / Math.PI;
-            dist = dist * 60 * 1.1515;
-            return dist;
-        }
-    }
+        </View>
+    );
 }
 
-// tslint:disable-next-line: typedef
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -167,7 +173,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'flex-start'
+        justifyContent: 'flex-start',
     },
     hint: {
         backgroundColor: '#eee',
@@ -177,13 +183,13 @@ const styles = StyleSheet.create({
         width: '100%',
         margin: 5,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     step: {
         flex: 0,
         height: 50,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     map: {
         margin: 5,
@@ -191,7 +197,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexGrow: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     action: {
         width: '100%',
@@ -199,6 +205,6 @@ const styles = StyleSheet.create({
         flex: 0,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'flex-end'
-    }
+        justifyContent: 'flex-end',
+    },
 });
